e7343a55377cff7dd60843ef197d88f48d199699
Annalise Haynes <annalise@adorable.io>
2017-04-11 20:33:38 -0700

    Validations for Event attributes

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,2 +1,16 @@
 class Event < ApplicationRecord
+  include ActiveModel::Validations
+
+  class TimeslotValidator < ActiveModel::EachValidator
+    def validate_each(record, attribute, value)
+      unless value.respond_to?(:%) && (value % 15.minutes).zero?
+        record.errors.add attribute, "is not an even 15 minute interval"
+      end
+    end
+  end
+
+  validates :title, presence: true, format: /\S+/
+  validates :duration, presence: true, numericality: { greater_than_or_equal_to: 15.minutes }, timeslot: true
+  validates :period, presence: true, numericality: { greater_than_or_equal_to: 0 }
+  validates :scheduled_time, allow_nil: true, numericality: { greater_than_or_equal_to: 0 }, timeslot: true
 end
--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -1,5 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe Event, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  fdescribe "validations" do
+    valid_timeslots = [15.minutes, 30.minutes, 1.day, "900"]
+    invalid_timeslots = [1, -1, -15.minutes, 16.minutes, "1", "15.minutes"]
+
+    it { should validate_presence_of(:title) }
+    it { should validate_presence_of(:duration) }
+    it { should validate_presence_of(:period) }
+    it { should validate_numericality_of(:duration).is_greater_than_or_equal_to(15.minutes) }
+    it { should validate_numericality_of(:period).is_greater_than_or_equal_to(0) }
+    it { should validate_numericality_of(:scheduled_time).is_greater_than_or_equal_to(0) }
+    it { should allow_value("foo").for(:title) }
+    it { should_not allow_value("", " ").for(:title) }
+    it { should allow_values(*valid_timeslots).for(:duration) }
+    it { should_not allow_values(*invalid_timeslots).for(:duration) }
+    it { should_not allow_values(0, "").for(:duration) }
+    # TODO: Something more like timestamps for these...
+    it { should allow_values(nil, 0, "").for(:scheduled_time) }
+    it { should allow_values(*valid_timeslots).for(:scheduled_time) }
+    it { should_not allow_values(*invalid_timeslots).for(:scheduled_time) }
+  end
 end
