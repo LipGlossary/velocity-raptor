7800ea07f5dd75aa4c642d965b8b6c95d6d30abb
Annalise Haynes <annalise@adorable.io>
2017-04-18 19:10:35 -0700

    Adds associations and shouldas to Event

--- app/models/event.rb
+++ app/models/event.rb
@@ -3,7 +3,13 @@ class Event < ApplicationRecord
 
   alias_attribute :start_time, :scheduled_time
 
+  has_many :blocks, dependent: :destroy
+
   validates :period, presence: true, numericality: { greater_than_or_equal_to: 0 }
   validates :scheduled_time, allow_nil: true, timeslot: true
   validates :title, presence: true, format: /\S+/
+
+  def start_times
+    blocks.pluck(:start_time)
+  end
 end
