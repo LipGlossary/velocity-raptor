ab50fa6fdd413fb20a760ba30d0eede8ba08ef46
Annalise Haynes <annalise@adorable.io>
2017-04-18 22:08:33 -0700

    Adds associations and validations for Block

--- app/models/block.rb
+++ app/models/block.rb
@@ -1,3 +1,10 @@
 class Block < ApplicationRecord
+  include Timeslotted
+
+  default_scope { order(start_time: :asc) }
+
   belongs_to :event
+
+  validates :start_time, presence: true
+  validates :title, allow_nil: true, format: /\S+/
 end
--- spec/models/block_spec.rb
+++ spec/models/block_spec.rb
@@ -1,5 +1,28 @@
 require 'rails_helper'
+require 'shared_examples/timeslotted'
 
 RSpec.describe Block, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it_behaves_like "Timeslotted"
+
+  describe "table" do
+    # it { should have_db_column(:type).of_type(:string) }
+    # it { should have_db_index(:type) }
+    it { should have_db_column(:duration).of_type(:integer) }
+    it { should have_db_column(:event_id).of_type(:integer) }
+    it { should have_db_column(:start_time).of_type(:integer).with_options(null: false) }
+    it { should have_db_column(:title).of_type(:string) }
+    it { should have_db_index(:event_id) }
+  end
+
+  describe "associations" do
+    it { should belong_to(:event) }
+  end
+
+  describe "validations" do
+    it { should validate_presence_of(:start_time) }
+    it { should allow_value(0).for(:start_time) }
+    it { should_not allow_values(nil, "").for(:start_time) }
+    it { should allow_values(nil, "foo").for(:title) }
+    it { should_not allow_value("").for(:title) }
+  end
 end
