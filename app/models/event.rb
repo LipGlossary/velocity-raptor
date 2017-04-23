eb1e1541488d953b0aa3b72ced8a08ecf0674619
Annalise Haynes <annalise@adorable.io>
2017-04-23 16:26:04 -0700

    Create/destroy blocks when event is un-/scheduled

--- app/models/event.rb
+++ app/models/event.rb
@@ -8,7 +8,25 @@ class Event < ApplicationRecord
   validates :period, presence: true, numericality: { greater_than_or_equal_to: 0 }
   validates :title, presence: true, format: /\S+/
 
+  after_save :reschedule_blocks, if: -> { scheduled_time_changed? }
+  after_destroy :unschedule_blocks
+
   def start_times
     blocks.pluck(:start_time)
+  end
+
+  private
+
+  def unschedule_blocks
+    blocks.destroy_all
+  end
+
+  def schedule_blocks
+    blocks.create(start_time: scheduled_time)
+  end
+
+  def reschedule_blocks
+    unschedule_blocks
+    schedule_blocks
   end
 end
